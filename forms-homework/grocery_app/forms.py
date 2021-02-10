7a6cd94c55c560cf6a7150c933a1501c35548de5
Tristan Thompson <tristan15thompson@gmail.com>
2021-02-09 18:58:37 -0800

    [ADD] completed forms TO-DO

--- forms-homework/grocery_app/forms.py
+++ forms-homework/grocery_app/forms.py
@@ -10,6 +10,10 @@ class GroceryStoreForm(FlaskForm):
     # - title - StringField
     # - address - StringField
     # - submit button
+    title = StringField('Title', validators=[DataRequired(), Length(min=3, max=80)])
+    address = StringField('Address', validators=[DataRequired(), Length(min=3, max=150)])    
+    submit = SubmitField('Submit')
+
     pass
 
 class GroceryItemForm(FlaskForm):
@@ -22,4 +26,10 @@ class GroceryItemForm(FlaskForm):
     # - photo_url - StringField (use a URL validator)
     # - store - QuerySelectField (specify the `query_factory` param)
     # - submit button
+    name = StringField('Name', validators=[DataRequired(), Length(min=3, max=80)])
+    price = FloatField('Price', validators=[DataRequired())
+    category = SelectField('Category', choices=ItemCategory.choices())
+    photo_url = StringField('Photo', validators=[DataRequired(), Length(min=3, max=80)])
+    store = QuerySelectField('Store', query_factory=lambda: GroceryStore.query)    
+    submit = SubmitField('Submit')
     pass
