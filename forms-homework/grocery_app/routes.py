61eff1be162771dd7df5ee91a68d2c5dcfae92f9
Tristan Thompson <tristan15thompson@gmail.com>
2021-02-09 19:09:24 -0800

    [EDIT] completed new_item route, fixed minor syntax bugs

--- forms-homework/grocery_app/routes.py
+++ forms-homework/grocery_app/routes.py
@@ -29,14 +29,14 @@ def new_store():
     # - redirect the user to the store detail page.
     if form.validate_on_submit():
       new_store = GroceryStore(
-        title = form.title.data()
-        address = form.address.data()
-        items = form.items.data()
+        title = form.title.data,
+        address = form.address.data,
+        items = form.items.data
       )
       db.session.add(new_store)
       db.session.commit()
 
-      flash("New store was created successfully"
+      flash("New store was created successfully")
       return redirect(url_for('main.store_detail'), store_id=new_store.id) )
 
     # TODO: Send the form to the template and use it to render the form fields
@@ -45,14 +45,27 @@ def new_store():
 @main.route('/new_item', methods=['GET', 'POST'])
 def new_item():
     # TODO: Create a GroceryItemForm
+    form = GroceryItemForm()
 
     # TODO: If form was submitted and was valid:
     # - create a new GroceryItem object and save it to the database,
     # - flash a success message, and
     # - redirect the user to the item detail page.
+    if form.validate_on_submit():
+      new_item = GroceryItem(
+        name = form.name.data,
+        price = form.price.data,
+        category = form.category.data,
+        photo_url = form.photo_url.data
+      )
+      db.session.add(new_item)
+      db.session.commit()
+
+      flash("New item was created successfully")
+      return redirect(url_for('main.item_detail'), item_id=new_item.id) )
 
     # TODO: Send the form to the template and use it to render the form fields
-    return render_template('new_item.html')
+    return render_template('new_item.html', form=form)
 
 @main.route('/store/<store_id>', methods=['GET', 'POST'])
 def store_detail(store_id):
