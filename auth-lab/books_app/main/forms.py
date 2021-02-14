16fa5072bb428406fb39b24aa60923f68011a745
Tristan Thompson <tristan15thompson@gmail.com>
2021-02-13 21:52:19 -0800

    [ADD] added create author route and Jinja2 template

--- auth-lab/books_app/main/forms.py
+++ auth-lab/books_app/main/forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, DateField, SelectField, SubmitField
+from wtforms import StringField, PasswordField, DateField, SelectField, SubmitField, TextAreaField
 from wtforms.ext.sqlalchemy.fields import QuerySelectField, QuerySelectMultipleField
 from wtforms.validators import DataRequired, Length, ValidationError
 from books_app.models import Audience, Book, Author, Genre, User
@@ -20,10 +20,9 @@ class BookForm(FlaskForm):
 class AuthorForm(FlaskForm):
     """Form to create an author."""
 
-    # TODO: Fill out the fields in this class for:
-    # - the author's name
-    # - the author's biography (hint: use a TextAreaField)
-    # - a submit button
+    name = StringField('Author Name', validators=[DataRequired(), Length(min=3, max=80)])
+    biography = TextAreaField('Biography', validators=[DataRequired(), Length(min=3, max=80)])
+    submit = SubmitField('Submit')
 
     # STRETCH CHALLENGE: Add more fields here as well as in `models.py` to
     # collect more information about the author, such as their birth date,
