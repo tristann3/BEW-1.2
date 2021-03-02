02eb08a9d6626a538c21b2de9a577afae9534ebe
Tristan Thompson <tristan15thompson@gmail.com>
2021-03-01 16:10:55 -0800

    [ADD] Users can Sign Up, Login and Logout
    Created Jinja2 Templates for Sign-In, Login and Homepages. Auth routes now include the routes and forms needed to Login and Create a User. User Model added.

--- BEW-1.2-Final-Project/students_app/main/routes.py
+++ BEW-1.2-Final-Project/students_app/main/routes.py
@@ -1,6 +1,17 @@
-from flask import Blueprint
+from flask import Blueprint, request, render_template, redirect, url_for, flash
+from flask_login import login_user, logout_user, login_required, current_user
+from datetime import date, datetime
+from students_app.models import  User
+# from students_app.main.forms import BookForm, AuthorForm, GenreForm
+from students_app import bcrypt
 
+# Import app and db
 from students_app import app, db
 main = Blueprint('main', __name__)
 
 # Create your routes here.
+@main.route('/')
+def homepage():
+    all_users = User.query.all()
+    return render_template('home.html',
+        all_users=all_users)
